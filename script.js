9ca65d9b50a3e5b853b7d83f74b2436f98ccae0f
zettacamp-redy-pamungkas <redy.surya@zettacamp.pro>
2022-10-01 07:44:19 +0700

    move style to style.css and seperate tableArr to script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+// Add Dummy Data to table
+const tableArr = [
+    [
+        'Lobak',
+        './img/turnip_pexels.jpeg',
+        1.56,
+        'fruit'
+    ],
+    [
+        'Anggur',
+        './img/grape_pexels.jpeg',
+        2.21,
+        'fruit'
+    ],
+    [
+        'Kopi',
+        './img/coffee_pexels.jpeg',
+        1.69,
+        'dairy'
+    ],
+];
