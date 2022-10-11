276fd3df1a134447e85ad54318ed44f67f179b3d
zettacamp-redy-pamungkas <redy.surya@zettacamp.pro>
2022-10-11 10:58:52 +0700

    Add two function bookPriceAfterDiscount and bookPriceAterTax

---
+++ app.js
@@ -0,0 +1,28 @@
+const buku = {
+    title: 'Untitled Book',
+    author: 'Unfamous Author',
+    price: 10000,
+    onSale: false,
+    discount: 20,
+    tax: 11
+};
+
+// Harga buku setelah di diskon
+function bookPriceAfterDiscount(book) {
+    let bookPriceAfterDiscount;
+    if (book.onSale) {
+        console.log(`Book '${book.title}' is on Sale`);
+        bookPriceAfterDiscount = book.price * (book.discount / 100);
+        book.priceAfterDiscount = book.price - bookPriceAfterDiscount;
+        console.log(`Your Saving: ${bookPriceAfterDiscount}`);
+    } else {
+        console.log(`Sorry, there no sale on this '${book.title}' book`)
+    }
+};
+
+// Harga buku setelah di pajakin
+function bookPriceAfterTax(book) {
+    let price = book.priceAfterDiscount || book.price;
+    book.taxPrice = price * book.tax / 100;
+    book.finalPrice = price + book.taxPrice;
+};
