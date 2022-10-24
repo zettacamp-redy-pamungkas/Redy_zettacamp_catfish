4f10f37c14cf4b56b35f70fb6ae44299fc2a7b2a
zettacamp-redy-pamungkas <redy.surya@zettacamp.pro>
2022-10-24 15:20:49 +0700

    Add CRUD books

--- server.js
+++ server.js
@@ -3,13 +3,155 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// mongoose
+const mongoose = require('mongoose')
+const dbName = 'bookStore';
+
+// Book, Author
+const Book = require('./models/book');
+const Author = require('./models/author');
+
+// mongoose connections
+mongoose.connect(`mongodb://localhost:27017/${dbName}`)
+    .then(() => {
+        console.log('MongoDB connection Open');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+// funtion getRandomMinMax
+function getRandomMinMax(min, max) {
+    if (min > max) {
+        [min, max] = [max, min];
+    }
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// BOOK ROUTE
 // GET '/books/' route
 app.get('/books', async (req, res) => {
-    res.json({
-        status: 'ok',
-        message: 'Hello, World'
-    });
+    let allBooks = await Book.find({}).populate('author', 'firstName lastName');
+    res.json(allBooks);
 });
+
+// GET '/books/detail/:id' route
+app.get('/books/detail/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const book = await Book.findById(id).populate('author', 'firstName lastName dob');
+        if (book) {
+            res.json(book);
+        } else {
+            next(new Error(`Book with id ${id} not found.`))
+        }
+    } catch (err) {
+        next(err)
+    }
+});
+
+// POST 'books/new' route
+app.post('/books/new', express.urlencoded({ extended: true }), async (req, res, next) => {
+    try {
+        let { book } = req.body
+        const randomPublished = new Date(`${getRandomMinMax(2000, 2012)}-${getRandomMinMax(1, 12)}-${getRandomMinMax(1, 30)}`);
+        book.datePublished = randomPublished;
+        // check author
+        if (book.author) {
+            let authors_id = await Author.find({});
+            authors_id = authors_id.map((el) => { return el.id })
+            if (authors_id.includes(book.author)) {
+                book = new Book(book);
+                const author = await Author.findById(book.author);
+                author.books.push(book);
+                await Promise.all([author.save(), book.save()]);
+                res.json({
+                    status: 'ok',
+                    message: book
+                });
+            } else {
+                next(new Error('Author Id not found.'))
+            }
+        } else {
+            book = new Book(book);
+            await book.save()
+            res.json({
+                status: 'ok',
+                message: book
+            })
+        }
+    } catch (err) {
+        next(err)
+    }
+});
+
+// PUT '/buku/detail/:id' route
+app.put('/books/detail/:id', express.urlencoded({ extended: true }), async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { book } = req.body;
+        book.updatedAt = new Date();
+        const updatedBook = await Book.findByIdAndUpdate(id, book, { new: true });
+    
+        res.json({
+            status: 'ok',
+            message: updatedBook
+        });
+    } catch (err) {
+        next(err)
+    }
+});
+
+// DELETE '/buku/detail/:id' route
+app.delete('/books/detail/:id', async(req, res, next) => {
+    try {
+        const { id } = req.params;
+        await Book.findByIdAndDelete(id);
+        res.json({
+            status: 'ok',
+            message: `Book with id ${id} has been deleted`
+        })
+    } catch (err) {
+        next(err)
+    }
+})
+
+
+
+// END BOOK ROUTE
+
+// AUTHOR ROUTE
+// GET '/authors' route
+app.get('/authors', async (req, res) => {
+    const allAuthors = await Author.find({}).populate('books', 'title');
+    res.json(allAuthors);
+});
+
+// GET '/authors/detail/:id' route
+app.get('/authors/detail/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const author = await Author.findById(id).populate('books');
+        res.json(
+            author
+        )
+    } catch (err) {
+        next(err)
+    }
+})
+
+// END AUTHOR ROUTE
+
+
+// Express Error Handler
+app.use((err, req, res, next) => {
+    const { message } = err;
+    res.json({
+        status: 400,
+        message
+    })
+})
+
 
 // Express Listen
 app.listen(port, () => {
