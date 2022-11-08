3e42e5958e0be350bb54a04d760fb8271798827f
zettacamp-redy-pamungkas <redy.surya@zettacamp.pro>
2022-11-08 14:43:20 +0700

    Add mongoose email validation

--- models/user.model.js
+++ models/user.model.js
@@ -21,7 +21,8 @@ const userSchema = new Schema({
     email: {
         type: String,
         trim: true,
-        unique: true
+        unique: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
     },
     password: {
         type: String,
